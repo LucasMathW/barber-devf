bd04a073cd92236aa89c4c5847f257985eff3864
LucasMathW <lucasag0408@outlook.com>
2021-03-22 19:01:14 -0400

    Adding properties to the object user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import User from '../models/User';
 import CreateUserService from '../services/createUserService';
 
 const usersRouter = Router();
@@ -10,11 +11,19 @@ const usersRouter = Router();
 //   return response.json(appointments);
 // });
 
+interface UserProps{
+  name: string,
+  email: string
+  password?: string
+}
+
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body
     const createUser = new CreateUserService();
-    const user = await createUser.execute({name, email, password});
+    const user: UserProps = await createUser.execute({name, email, password});
+
+    delete user.password;
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
