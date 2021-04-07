b9d02c441600527c8f285757b4004e19605e3a11
LucasMathW <lucasag0408@outlook.com>
2021-04-07 17:37:48 -0400

    Global errors with express-asybc-erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -16,16 +16,12 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUser = new CreateUserService();
-    const user: UserProps = await createUser.execute({ name, email, password });
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
+  const user: UserProps = await createUser.execute({ name, email, password });
 
-    delete user.password;
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  delete user.password;
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -33,20 +29,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
-      const user: UserProps = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatarService = new UpdateUserAvatarService();
+    const user: UserProps = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
-    return response.json({ ok: true });
+    return response.json(user);
   },
 );
 
