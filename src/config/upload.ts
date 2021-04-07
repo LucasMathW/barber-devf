47bd7cc7c54252db1db17149c483d38221efd271
LucasMathW <lucasag0408@outlook.com>
2021-04-07 15:22:23 -0400

    add image path to a variable

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import multer from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
+const tempFold = path.resolve(__dirname, '..', '..', 'temp');
+
 export default {
+  directory: tempFold,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
+    destination: tempFold,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const filename = `${fileHash}-${file.originalname}`;
