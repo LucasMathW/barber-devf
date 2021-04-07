8139134950064cf970c3f90095481f87b14d6a28
LucasMathW <lucasag0408@outlook.com>
2021-04-07 15:26:49 -0400

    Import UpdateUserAvarService class to users router

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/createUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 interface UserProps {
   name: string;
@@ -32,7 +33,19 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+      const user: UserProps = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
     return response.json({ ok: true });
   },
 );
