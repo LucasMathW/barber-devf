55c63824395f563b9eb1ff54bfd767ea1466d593
LucasMathW <lucasag0408@outlook.com>
2021-08-03 20:55:32 -0400

    hash provider

---
+++ src/modules/users/provider/index.ts
@@ -0,0 +1,5 @@
+import { container } from 'tsyringe';
+import IHashProvider from './hashProvider/models/IHashProvider';
+import BCryptHashProvider from './hashProvider/implementations/BCryptHashProvider';
+
+container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
