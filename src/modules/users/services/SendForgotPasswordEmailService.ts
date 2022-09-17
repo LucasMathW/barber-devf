e06b9ef9eb28448a9e5c35c849c09ae88d1110a0
Lucas Matheus <lucasag0408@outlook.com>
2022-09-17 15:49:39 -0400

    removing some imports and rename soma variables

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,8 @@
 import { inject, injectable } from 'tsyringe';
-import IEmailProvider from '@shared/container/providers/EmailProvider/fakes/fakeEmailProvider';
+import IEmailProvider from '@shared/container/providers/EmailProvider/models/IEmailProvides';
 import AppError from '@shared/errors/AppError';
+import path from 'path';
 import IUsersRepository from '../repositories/IUsersRepository';
-// import FakeUserTokenRepository from '../repositories/fakes/FakeUserTokensRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
@@ -23,15 +23,35 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const userExist = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!userExist) {
+    if (!user) {
       throw new AppError('user does not exist');
     }
 
-    this.userTokensRepository.generated(userExist.id);
+    const { token } = await this.userTokensRepository.generated(user.id);
 
-    this.emailProvider.sendMail(email, 'pedido de recuperação de senha');
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'resetPassword.hbs',
+    );
+
+    await this.emailProvider.sendMail({
+      to: {
+        name: user.email,
+        email: user.email,
+      },
+      subject: '[Go Barber] Password Recovery',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
