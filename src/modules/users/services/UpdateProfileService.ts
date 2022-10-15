8e05930f9f5174f46436ee7fb60fb8385bdcc4ad
Lucas Matheus <lucasag0408@outlook.com>
2022-10-15 17:09:40 -0400

    service that update profile

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,70 @@
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../provider/hashProvider/models/IHashProvider';
+
+interface Request {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    old_password,
+    password,
+  }: Request): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdateEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError('email already in user');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError('You need the old password');
+    }
+
+    if (password && old_password) {
+      const hashedPassword = await this.hashProvider.compareHash(
+        user.password,
+        old_password,
+      );
+
+      if (!hashedPassword) {
+        throw new AppError('old password is incorrect');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateProfileService;
