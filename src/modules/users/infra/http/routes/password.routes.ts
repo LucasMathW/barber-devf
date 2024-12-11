1ca589a7a2fc87e485b0a5022026c9bdf2af7331
Lucas Matheus <lucasag0408@outlook.com>
2024-12-11 16:35:36 -0400

    add validate request data with celebrate

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,13 +1,33 @@
 import { Router } from 'express';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
+import { celebrate, Segments, Joi } from 'celebrate';
+import { uuid } from 'uuidv4';
 
 const passwordRouter = Router();
 
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.sendForgotPassword);
-passwordRouter.post('/reset', resetPasswordController.resetPassword);
+passwordRouter.post('/forgot',
+  celebrate({
+    [Segments.BODY] : {
+      email: Joi.string().email().required(),
+    }
+  }),
+  forgotPasswordController.sendForgotPassword
+);
+
+
+passwordRouter.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token : Joi.string().uuid().required(),
+      password : Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref("password"))
+    }
+  }),
+  resetPasswordController.resetPassword
+);
 
 export default passwordRouter;
