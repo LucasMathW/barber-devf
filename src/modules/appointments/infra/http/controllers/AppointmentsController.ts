d12fa78a06ad93660382564dfac9952250ac16e0
Lucas Matheus <lucasag0408@outlook.com>
2024-12-11 15:59:26 -0400

    remove parseDate

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -5,15 +5,15 @@ import { container } from 'tsyringe';
 
 export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
     const { provider_id, date } = request.body;
-
-    const parserDate = parseISO(date);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date: parserDate,
+      date,
       provider_id,
+      user_id,
     });
 
     return response.json(appointment);
