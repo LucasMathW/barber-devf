2cd226e301fbc0e50ce700e3c55004ef92c4de1f
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 15:07:47 -0400

    change name variable

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -25,7 +25,7 @@ export default class ProfileController {
 
   public async update(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
-    const { name, email, old_password, password } = request.body;
+    const { name, email, oldPassword, password } = request.body;
 
     const updateProfile = container.resolve(UpdateProfileService);
 
@@ -33,7 +33,7 @@ export default class ProfileController {
       user_id,
       name,
       email,
-      old_password,
+      oldPassword,
       password,
     });
 
