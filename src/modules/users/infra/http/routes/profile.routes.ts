4cddf9bbe1432d2f7fcd0750a5ea0701c89e94cb
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 15:11:29 -0400

    adding celebrate for validate request data

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -2,13 +2,25 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProfileController from '../controllers/ProfileController';
+import { celebrate , Segments, Joi } from 'celebrate';
 
 const profileRouter = Router();
 
 const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
-profileRouter.put('/update', profileController.update);
+
+profileRouter.put('/update', celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      oldPassword: Joi.string(),
+      password: Joi.string(),
+      passwordConfirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }), profileController.update
+);
+
 profileRouter.get('/show', profileController.show);
 
 export default profileRouter;
