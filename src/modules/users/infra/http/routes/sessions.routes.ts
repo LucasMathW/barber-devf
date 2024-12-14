ae5afd8f310cf9164a14daa4a8f6f901734ff3e5
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 15:11:53 -0400

    adding celebrate for validate request data

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import SessionsController from '../controllers/SessionsController';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 const sessionsRouter = Router();
 const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', sessionsController.authenticate);
+sessionsRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.authenticate
+);
 
 export default sessionsRouter;
