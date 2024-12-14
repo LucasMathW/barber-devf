5c68fdf6dc95057d3549765d2b5f688a20fc3c5a
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 15:37:17 -0400

    Injecting cache provider dependence!

--- src/modules/users/services/createUserService.ts
+++ src/modules/users/services/createUserService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../provider/hashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface Request {
   name: string;
@@ -18,6 +19,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -34,6 +38,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    this.cacheProvider.invalidatePrefix('providers-list')
 
     return user;
   }
