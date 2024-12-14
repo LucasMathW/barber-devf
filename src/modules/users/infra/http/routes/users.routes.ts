6f00f612538b4aa4e000ba4128b2c74ee31edbe6
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 15:12:19 -0400

    adding celebrate for validate request data

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -4,14 +4,24 @@ import uploadConfig from '@config/upload';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UsersController from '../controllers/UsersController';
 import UpdateAvatarController from '../controllers/UpdateAvatarController';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const updateAvatarController = new UpdateAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create
+);
 
 usersRouter.patch(
   '/avatar',
