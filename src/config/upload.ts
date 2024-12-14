2a3f40a7510638f68654ac15a549b5ccb59123b1
Lucas Matheus <lucasag0408@outlook.com>
2024-12-14 16:00:11 -0400

    creating uploadConfig interface

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,49 @@
-import multer from 'multer';
+import multer, {StorageEngine} from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
-const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
-  tempFolder,
-  uploadsFolder: path.resolve(tempFolder, 'uploads'),
+  driver: process.env.STORAGE_DRIVER,
 
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-      return callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
+
+        return callback(null, filename);
+      },
+    })
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'gobarber',
     },
-  }),
-};
+  },
+} as IUploadConfig;
